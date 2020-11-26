9b8550ac58ea83f7d82747c6e2d9e69c1418afdc
unknown <luc.gireaud@gmail.com>
2020-11-26 11:36:07 +0100

    encodage fix 1

--- src/main/java/Main/PizzaTestDrive.java
+++ src/main/java/Main/PizzaTestDrive.java
@@ -1,11 +1,14 @@
 package Main;
 
+import java.nio.charset.Charset;
+
 import Fabrique.Pizzeria;
 import Fabrique.PizzeriaFactory;
 import Pizza.Pizza;
 
 public class PizzaTestDrive {
     public static void main(String[] args) {
+    	System.setProperty("file.encoding", "UTF-8");
         Pizzeria boutiqueBrest = PizzeriaFactory.getInstance().creer("Brest");
         Pizzeria boutiqueStrasbourg = PizzeriaFactory.getInstance().creer("Strasbourg");
 
@@ -13,8 +16,7 @@ public class PizzaTestDrive {
         System.out.println("JMB a commandé une " + pizza.getNom() + "\n");
 
         pizza = boutiqueStrasbourg.commanderPizza("fromage");
-        System.out.println("JMI a commandé une " + pizza.getNom() + "\n");
-   
+        System.out.println(new String("JMI a commandé une ".getBytes(), Charset.forName("UTF-8")) + pizza.getNom() + "\n");
         
         
         /**
